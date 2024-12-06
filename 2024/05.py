5c0441a0ea63c80fa7789f6925e260dc41b1c2c9
Carter Tran <carter.tran@gmail.com>
2024-12-05 20:57:55 -0800

    unfinished day 5

--- 2024/05.py
+++ 2024/05.py
@@ -8,5 +8,54 @@ lines = input.split('\n')
 
 ans = ans_ = 0
 
+disallowed = defaultdict(list)
+
+for line in lines:
+    if "|" in line:
+        x, y = line.split('|')
+        x, y = int(x), int(y)
+        disallowed[y].append(x)
+    elif ',' in line:
+        update = [int(num) for num in line.split(',')]
+        valid = True
+        for i in range(len(update) - 1):
+            d = disallowed[update[i]]
+            remaining = set(update[i+1:])
+            if any(n in remaining for n in d):
+                valid = False
+                break
+        if valid:
+            ans += update[len(update) // 2]
+
+print(ans)
+pyperclip.copy(ans)
+
+def is_valid(update):
+    valid = True
+    for i in range(len(update) - 1):
+        d = disallowed[update[i]]
+        remaining = set(update[i+1:])
+        if any(n in remaining for n in d):
+            valid = False
+            break
+    return valid
+
+for line in lines:
+    if "|" in line:
+        x, y = line.split('|')
+        x, y = int(x), int(y)
+        disallowed[y].append(x)
+    elif ',' in line:
+        update = [int(num) for num in line.split(',')]
+        while not is_valid(update):
+            for i in range(len(update) - 1):
+                d = disallowed[update[i]]
+                remaining = set(update[i+1:])
+                for n in d:
+                    if n in remaining:
+                        j = update.index(n)
+                        update[i], update[j] = update[j], update[i]
+        ans += update[len(update) // 2]
+
 print(ans)
 pyperclip.copy(ans)
